3d72c500049a294af037733d059c674846046208
tagawashouta <tagawashouta@localhost>
2024-11-01 16:46:15 +0900

    java導入サンプル

---
+++ src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//名前
+		String name = "田川 翔太";
+		//年齢
+		int age = 28;
+		//趣味
+		String hobby = "サウナ";
+		
+		/*
+		 * 自己紹介の説明文
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
