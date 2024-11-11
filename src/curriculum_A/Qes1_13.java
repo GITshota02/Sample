75274196463b015236a27e82a71271bf76fcd99b
tagawashouta <tagawashouta@localhost>
2024-11-11 22:53:47 +0900

    課題1-3まで進行致しました。

---
+++ src/curriculum_A/Qes1_13.java
@@ -0,0 +1,27 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//バイト型宣言及び初期化と初期値の代入
+		byte by = 10;
+		//短整数型宣言及び初期化と初期値の代入
+		short sh = 100;
+		//整数型宣言及び初期化と初期値の代入
+		int in = 1000;
+		//長整数型宣言及び初期化と初期値の代入
+		long lo = 10000;
+		//単精度浮動小数点数型宣言及び初期化と初期値の代入
+		float fl = 9.5F;
+		//倍精度浮動小数点数型宣言及び初期化と初期値の代入
+		double dou = 10.5;
+		//文字型宣言及び初期化と初期値の代入
+		char ch = 'a';
+		//文字列型宣言及び初期化と初期値の代入
+		String st = "ハロー";
+		//ブーリアン型宣言及び初期化と初期値の代入
+		boolean bo = true;
+	}
+
+}
